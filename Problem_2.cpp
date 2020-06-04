1ada0e666eba4d257ec4e3bc18800a2a0818922f
ScottKang0727 <55644242+ScottKang0727@users.noreply.github.com>
2020-06-04 10:59:56 -0700

    Add files via upload

---
+++ Problem_2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int x) {
+    long long int reverse = 0;
+        
+        while(x != 0) {
+            reverse = reverse*10;
+            reverse += x%10;
+            x /= 10;
+        }
+        return (int)reverse == reverse ? (int)reverse : 0;
+    }
+};
